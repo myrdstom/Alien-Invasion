3f27f46a1b3d70cc713c43da6177987004c04256
myrdstom <nserekopaul@gmail.com>
2018-03-02 16:47:43 +0300

    First game with pygame, first commit

---
+++ game_stats.py
@@ -0,0 +1,19 @@
+class GameStats():
+    """Track statistics for alien invasion."""
+    def __init__(self, ai_settings):
+        """Initialize statistics"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+
+        #High Score should never be reset.
+        self.high_score = 0
+
+
+        #Start Alien invasion in an inactive state.
+        self.game_active = False
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
